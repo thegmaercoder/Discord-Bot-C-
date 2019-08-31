35ff786a00e6a1788a1452297bcac9a52f4f5219
thegmaercoder <54740407+thegmaercoder@users.noreply.github.com>
2019-08-31 14:46:33 +0400

    Update Sevice.cs

--- Sevice.cs
+++ Sevice.cs
@@ -24,90 +24,10 @@ namespace ConsoleApp4
 {
   public class SC :  ModuleBase<SocketCommandContext>
     {
-        [Command("rand")]
-        [Summary("Echoes a message.")]
-        public async Task SayHello()
-        {
-            int random = new Random().Next();
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.WithTitle("Randomizer");
-            eb.WithColor(Color.DarkGreen);
-            eb.WithCurrentTimestamp();
-      
-            eb.WithDescription($"Hello, your random number is {random}");
-
-            //       eb.WithAuthor($"Especially For You {Context.User.Username}", Context.User.GetAvatarUrl());
-
-
-            await Context.Channel.SendMessageAsync("", true, eb.Build());
-
-        }
-      
-        [Command("image"),Alias("im","gif")]
-        public async Task SendImage()
-        {
-            EmbedBuilder eb = new EmbedBuilder
-            {
-                Title = "GIPHY",
-                ImageUrl = "https://i.giphy.com/13gvXfEVlxQjDO.gif"
-            };
-
-            await ReplyAsync("", false, eb.Build());
-        }
-        [Command("imgen"), Alias("imageGenerator")]
-        public async Task SendImageGeneretion(string text = "Hello", string color = "white")
-        {
-            string html = "<style>\n  h1{\n color:" + color + ";\n  }\n </style>\n <h1>" + text + "</h1>";
-            var convertor = new HtmlToImageConverter
-            {
-                Height = 70,
-                Width = 200
-            };
-            var jpg = convertor.GenerateImage(html, NReco.ImageGenerator.ImageFormat.Jpeg);
-
-            await Context.Channel.SendFileAsync(new MemoryStream(jpg),"hello.jpg" ,"your image is");
-        }
-
+       
 [Command("vc"), Alias("VC")] 
-public async Task CP()
-        {/*
-            string json = "";
-            using(WebClient wc = new WebClient())
-            {
-                json = wc.DownloadString(new Uri("https://randomuser.me/api/?gender=" + personGender + "&nat=" + personGender));
-            }
-            var data = JsonConvert.DeserializeObject<dynamic>(json);
-
-            var first = data.result[0].name.first.toString();
-            var last = data.result[0].name.last.toString();
-            var Im = data.result[0].picture.large.toString();
-
-
-            EmbedBuilder eb = new EmbedBuilder();
-            EmbedFieldBuilder efb = new EmbedFieldBuilder();
-            efb.WithIsInline(true);
-            efb.WithName("First Name: ");
-            efb.WithValue(first);
-            efb.WithName("Last Name");
-            efb.WithValue(last);
-            eb.WithThumbnailUrl(Im);
-            eb.WithTitle("Generated Person");
-            eb.WithFields(efb);
-            await Context.Channel.SendMessageAsync("", false, eb.Build());
-           
-           
-             */
-
-            //var con = (Context.Client.GetChannel(558271718171475993) as SocketVoiceChannel);
-
-            // await con.ConnectAsync();
-            /*
-            await (Context.Guild.GetVoiceChannel(558271718171475993)).ConnectAsync(false, false, true);
-            con.Connected += Con_Connected;
-            con.Disconnected += Con_Disconnected;
-     
-             await con.SetSpeakingAsync(true);
-             */
+public async Task VC()
+        {
             IVoiceChannel channel = (Context.User as IVoiceState).VoiceChannel;
             if (channel == null)
             {
@@ -135,19 +55,14 @@ public async Task CP()
                             */
 
                 var direct =  client.CreatePCMStream(AudioApplication.Music);
-                using(var w = WebRequest.Create("https://www.youtube.com/watch?v=M3TmmqWBjO8").GetResponse().GetResponseStream())
-                {
-                    w.CopyTo(direct);
-
-                    direct.Flush();
-                }
-                /*
+      
+                
                 var ffmpeg = CreateStream(@"C:\audiofiles\lol.wav");
                 var stream = ffmpeg.StandardOutput.BaseStream;
                 await stream.CopyToAsync(direct);
 
                 await direct.FlushAsync();
-                */
+                
 
             }
         }
@@ -182,53 +97,7 @@ var Char=     await DM.SendMessageAsync("Disconnected! Error: "+arg.Message+"Exc
 
 
        
-      
-        [Command("kick"), RequireUserPermission(GuildPermission.KickMembers)]
-        public async Task Kick(IGuildUser userName, [Remainder]string reason = "No reason has been provided")
-        {
-            var user = Context.User as SocketGuildUser;
-            var roles = user as IGuildUser;
-            var roId = user.Guild.Roles.FirstOrDefault(x => x.Name == "Admin");
-
-            if(user.Roles.Contains(roId))
-            {
-                if (user.GuildPermissions.KickMembers)
-                {
-                    await userName.KickAsync(reason);
-                }
-            }
-            
-        }
-
-        [Command("ban"), RequireUserPermission(GuildPermission.KickMembers)]
-        public async Task Ban(IGuildUser userName, int days = 3,[Remainder]string reason = "No reason has been provided")
-        {
-            var user = Context.User as SocketGuildUser;
-            var roles = user as IGuildUser;
-            var roId = user.Guild.Roles.FirstOrDefault(x => x.Name == "Admin");
-
-            if (user.Roles.Contains(roId))
-            {
-                if (user.GuildPermissions.KickMembers)
-                {
-                    await userName.BanAsync(days,reason);
-                }
-            }
-
-        }
-        [Command("dm")]
-        public async Task TestOfDC([Remainder]string DmText = "Hello Bro!")
-        {
-            var DM = await Context.User.GetOrCreateDMChannelAsync();
-           await DM.SendMessageAsync(DmText);
-        }
-        
-      
-        
-
-        [Command("mul"), Alias("multiply", "vur", "умножить")]
-        public async Task mul(int a,int b) =>
-         await ReplyAsync($"Multiplied result equals to {a} * {b} = {a * b}");
+     
         
             
         
